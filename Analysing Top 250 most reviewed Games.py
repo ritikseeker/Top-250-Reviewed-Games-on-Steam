00cfadb9215219d3ac7011c304147509c90ad441
ritik <egiawofufuza@gmail.com>
2024-08-21 13:02:17 +0530

    changed minor things

--- Analysing Top 250 most reviewed Games.py
+++ Analysing Top 250 most reviewed Games.py
@@ -2,21 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-gameRankings = pd.read_excel("Top 250 Most Reviewed Games on Steam.xlsx")
+'''The games are assigned rank based on No. of Reviews (NOT based on Rating), WHY ? well in datasource they mentioned : 
+In this ranking games are sorted by number of reviews only, with complete disregard for rating. Total reviews usually have a strong correlation with total sales, so this provides a relative insight into best sellers of the year. 
+'''
+gameRankings = pd.read_excel("C:/Users/ritik/Documents/Projects/Web scraping project/Top-250-Reviewed-Games-on-Steam/Top 250 Most Reviewed Games on Steam.xlsx")
 # print(gameRankings.head())
 # print(gameRankings.tail())
 # print(gameRankings.columns)
 
 # Renaming columns
-gameRankings.rename(columns = {'Reviews on Steam':'Reviews','Rating on Steam':'Rating(out of 1)','Game Rank':'Rank','Game Name':'Title'}, inplace = True)
-# Top 5 Games by Rank
-# print(gameRankings.head())
+gameRankings.rename(columns = {'Reviews on Steam':'Reviews','Rating on Steam':'Rating(out of 100)','Game Rank':'Rank','Game Name':'Title'}, inplace = True)
+
+# Top 5 Games by Rank(no. of Reviews)
+print(gameRankings.head())
 
 # Statistical Summary on dataset
-# print(gameRankings.describe())
+print(gameRankings.describe())
 
 # Info on dataset
-# print(gameRankings.info())
+print(gameRankings.info())
 
 # Games within Ranking 5-10
 # print(gameRankings.loc[4:9])
@@ -25,7 +29,7 @@ gameRankings.rename(columns = {'Reviews on Steam':'Reviews','Rating on Steam':'R
 # print(gameRankings[gameRankings["Reviews"]> 1000000])
 
 # Game with > 1,000,000 Reviews and > 0.9 Ratings
-# print(gameRankings.loc[(gameRankings['Reviews']>1000000)&(gameRankings['Rating(out of 1)']>0.9)])
+# print(gameRankings.loc[(gameRankings['Reviews']>1000000)&(gameRankings['Rating(out of 100)']>0.9)])
 
 # Top rated game released in yr 2023
 # print(gameRankings.loc[gameRankings['Release Year'] == 2023].iloc[0])
@@ -33,19 +37,19 @@ gameRankings.rename(columns = {'Reviews on Steam':'Reviews','Rating on Steam':'R
 # Least rated game released in yr 2024
 # print(gameRankings.loc[gameRankings['Release Year'] == 2024].iloc[-1])
 
-# grouping by year
-avgGameRatingByYear = gameRankings.groupby(["Release Year"],as_index=False).agg({'Reviews':['mean'],'Rating(out of 1)':['mean'],'Title':['count']})
-avgGameRatingByYear.rename(columns={"Reviews": "Avg Reviews","Rating(out of 1)":"Avg Rating(out of 1)","Title":"Total Games Released","mean":"","count":""},inplace = True)
+# grouping by year 
+# avgGameRatingByYear = gameRankings.groupby(["Release Year"],as_index=False).agg({'Reviews':['mean'],'Rating(out of 100)':['mean'],'Title':['count']})
+# avgGameRatingByYear.rename(columns={"Reviews": "Avg Reviews","Rating(out of 100)":"Avg Rating(out of 100)","Title":"Total Games Released","mean":"","count":""},inplace = True)
 
 # Avg Review, Avg Rating, Total Games Released by Year Dataframe
 # print(avgGameRatingByYear)
-
+'''
 # Initialise the subplot function using number of rows and columns 
 figure, axis = plt.subplots(1, 3) 
   
 X = np.array(avgGameRatingByYear["Release Year"])
 Y1 = np.array(avgGameRatingByYear["Avg Reviews"])
-Y2 = np.array(avgGameRatingByYear["Avg Rating(out of 1)"])
+Y2 = np.array(avgGameRatingByYear["Avg Rating(out of 100)"])
 Y3 = np.array(avgGameRatingByYear["Total Games Released"])
 
 axis[0].bar(X, Y1) 
@@ -60,3 +64,4 @@ axis[2].set_title("Total Games Released yearwise")
 
 # Combine all operations & display 
 plt.show()
+'''
--- webscrapping.py
+++ webscrapping.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests,openpyxl
+from datetime import datetime, timedelta
 
 excel = openpyxl.Workbook()
 sheet = excel.active
 sheet.title = 'Top 250 Most Reviewed Games'
 sheet.append(['Game Rank','Game Name', 'Release Month','Release Year','Reviews on Steam','Rating on Steam'])
+current_time = datetime.now()
+monthDict = {'01':'Jan','02':'Feb','03':'Mar','04':'Apr','05':'May','06':'Jun','07':'Jul','08':'Aug','09':'Sep','10':'Oct','11':'Nov','12':'Dec'
+}
 
 try:
     source = 'https://steam250.com/reviews'
@@ -13,6 +17,7 @@ try:
     soup = BeautifulSoup(result.text, 'html.parser')
     movies1 = soup.find('div', class_='col1 main ranking')
     moviesList = movies1.find_all('div',id='1')
+
     for i in range(2,251):
         moviesList.extend(movies1.find_all('div',id=f'{i}'))
     for movie in moviesList:
@@ -20,11 +25,18 @@ try:
         rank = rankAndName[0]
         name = rankAndName[1]
         releaseMonthAndYear = movie.find('span',class_='date').a.text.split(" ")
-        releaseMonth = releaseMonthAndYear[1]
-        releaseYear = releaseMonthAndYear[2]
+        if len(releaseMonthAndYear[1]) != 3:
+            releaseDate = current_time - timedelta(days = int(releaseMonthAndYear[1]))
+            releaseDateString = str(releaseDate)
+            releaseYear = releaseDateString[:4]
+            releaseMonthNum = releaseDateString[5:7]
+            releaseMonth = monthDict[releaseMonthNum]
+        else:
+            releaseMonth = releaseMonthAndYear[1]
+            releaseYear = releaseMonthAndYear[2]
         numOfReviews = movie.find('span',class_='owners').text
-        rating = movie.find('span',class_='rating').text.split(" ")[-1]
-        print(rank,name,releaseMonth,releaseYear,numOfReviews,rating)
+        rating = movie.find('span',class_='rating').text.split(" ")[-1][:-1]
+        # print(rank,name,releaseMonth,releaseYear,numOfReviews,rating)
         sheet.append([rank,name,releaseMonth,releaseYear,numOfReviews,rating])
     
 except Exception as e:
